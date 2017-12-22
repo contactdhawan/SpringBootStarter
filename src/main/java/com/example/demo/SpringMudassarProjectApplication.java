6a462cb4141b84eed3f1f246f7f41c6de89e9f9f
Neeraj <contactdhawan@gmail.com>
2017-12-22 13:19:08 -0600

    adding run

--- src/main/java/com/example/demo/SpringMudassarProjectApplication.java
+++ src/main/java/com/example/demo/SpringMudassarProjectApplication.java
@@ -2,11 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+
 public class SpringMudassarProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringMudassarProjectApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(SpringMudassarProjectApplication.class, args);
+		run.getApplicationName();
 	}
 }
